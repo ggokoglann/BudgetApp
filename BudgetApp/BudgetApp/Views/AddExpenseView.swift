1d1a018b560f73401022929c37727ed07b6c86db
Gökhan Gökoğlan <119362605+ggokoglann@users.noreply.github.com>
2023-08-17 15:02:41 +0300

    Add files via upload

---
+++ BudgetApp/BudgetApp/Views/AddExpenseView.swift
@@ -0,0 +1,36 @@
+//
+//  AddExpenseViewController.swift
+//  BudgetApp
+//
+//  Created by Gökhan Gökoğlan on 12.08.2023.
+//
+
+import UIKit
+
+class AddExpenseView: UIViewController {
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var amount: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var mainViewModel = ExpenseViewModel.shared
+    var expenseAddedHandler: ((Expense) -> Void)?
+    var nameInput: String?
+    var amountInput: String?        
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        name.becomeFirstResponder()
+    }
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        guard let nameInput = name.text, let amountInputString = amount.text, let amountInput = Double(amountInputString) else {
+                    return
+                }
+                
+                let newItem = Expense(name: nameInput, cost: amountInput, date: Date())
+                mainViewModel.addExpense(newItem)
+                expenseAddedHandler?(newItem) 
+
+                dismiss(animated: true, completion: nil)
+        }
+    }
